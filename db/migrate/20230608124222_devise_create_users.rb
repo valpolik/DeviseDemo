ef69b151829607c6f3a772e13223220579a7db17
Valentin Polikarpov <valpolik@gmail.com>
2023-06-08 17:03:41 +0400

    Added name field to migration

--- db/migrate/20230608124222_devise_create_users.rb
+++ db/migrate/20230608124222_devise_create_users.rb
@@ -34,6 +34,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
 
 
       t.timestamps null: false
+
+
+
+      t.string :name
     end
 
     add_index :users, :email,                unique: true
